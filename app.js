58b61a67f4fed860dd393f33c54c1e2efd92a056
Andres Arbelaez <andresarbelaez@Andress-MacBook-Pro.local>
2016-10-13 20:34:38 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+$(function(){
+    var animationName="animated fadeInUp";
+    var animationEnd="webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+
+});
+$(document).ready(main);
+
+
